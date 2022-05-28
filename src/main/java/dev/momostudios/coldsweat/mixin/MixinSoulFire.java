09eca934a212913a67570e8e8a52400589c8e72d
Mikul <gamer3mc@gmail.com>
2022-05-28 19:00:26 -0400

    Add config option to disable cold soul fire

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinSoulFire.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinSoulFire.java
@@ -1,6 +1,7 @@
 package dev.momostudios.coldsweat.mixin;
 
 import dev.momostudios.coldsweat.ColdSweat;
+import dev.momostudios.coldsweat.config.ColdSweatConfig;
 import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoulFireBlock;
@@ -13,13 +14,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractFireBlock.class)
-public class MixinSoulfire
+public class MixinSoulFire
 {
     @Inject(method = "onEntityCollision(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V",
     at = @At("HEAD"), cancellable = true, remap = ColdSweat.REMAP_MIXINS)
     public void entityInside(BlockState state, World level, BlockPos pos, Entity entity, CallbackInfo ci)
     {
-        if (state.getBlock() instanceof SoulFireBlock)
+        if (state.getBlock() instanceof SoulFireBlock && ColdSweatConfig.getInstance().isSoulFireCold())
         {
             entity.extinguish();
             ci.cancel();
