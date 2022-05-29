2280ca187014b951667040ba2074f9dbf14387e3
Mikul <gamer3mc@gmail.com>
2022-05-29 13:20:09 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-sed -i "s/remapMixins = false/remapMixins = true/g" /home/michael/Programs/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
+sed -i "s/REMAP_MIXINS = false/REMAP_MIXINS = true/g" /home/michael/Programs/IdeaProjects/Cold-Sweat-Legacy/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
+chmod +x gradlew
 ./gradlew build
-sed -i "s/remapMixins = true/remapMixins = false/g" /home/michael/Programs/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
+sed -i "s/REMAP_MIXINS = true/REMAP_MIXINS = false/g" /home/michael/Programs/IdeaProjects/Cold-Sweat-Legacy/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
