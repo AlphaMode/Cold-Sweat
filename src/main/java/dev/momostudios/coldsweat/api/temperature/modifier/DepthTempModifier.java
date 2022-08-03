11abda35f24b32a33cc474716c75053f0dceff00
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:23:06 -0400

    Improve DepthTempModifier accuracy

--- src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/DepthTempModifier.java
+++ src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/DepthTempModifier.java
@@ -1,31 +1,56 @@
 package dev.momostudios.coldsweat.api.temperature.modifier;
 
+import dev.momostudios.coldsweat.util.entity.TempHelper;
 import dev.momostudios.coldsweat.util.math.CSMath;
 import dev.momostudios.coldsweat.util.world.WorldHelper;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.LightType;
 import dev.momostudios.coldsweat.api.temperature.Temperature;
-import dev.momostudios.coldsweat.config.ConfigCache;
+import dev.momostudios.coldsweat.util.config.ConfigCache;
 import net.minecraft.world.gen.Heightmap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class DepthTempModifier extends TempModifier
 {
     @Override
-    public Temperature getResult(Temperature temp, PlayerEntity player)
+    public Function<Temperature, Temperature> calculate(PlayerEntity player)
     {
+        if (player.world.getDimensionType().getHasCeiling()) return temp -> temp;
+
         double midTemp = (ConfigCache.getInstance().maxTemp + ConfigCache.getInstance().minTemp) / 2;
-        double depth = Math.max(0, WorldHelper.getGroundLevel(player.getPosition(), player.world) - player.getPosition().getY());
+        BlockPos playerPos = player.getPosition();
 
-        Map<Double, Double> valueMap = new HashMap<>();
-        valueMap.put(temp.get(), 0.5);
-        valueMap.put(CSMath.blend(midTemp, temp.get(), player.world.getLightFor(LightType.SKY, player.getPosition()), 0, 15), 2.0);
-        valueMap.put(CSMath.blend(temp.get(), midTemp, !WorldHelper.canSeeSky(player.world, player.getPosition()) ? depth : Math.max(0, depth - 10), 2, 20), 4.0);
+        List<Integer> lightLevels = new ArrayList<>();
+        List<Double> depthLevels = new ArrayList<>();
 
-        return new Temperature(CSMath.weightedAverage(valueMap));
+        for (BlockPos pos : WorldHelper.getNearbyPositions(playerPos, 50, 5))
+        {
+            double depth = Math.max(0, player.world.getHeight(Heightmap.Type.WORLD_SURFACE, pos.getX(), pos.getZ()) - playerPos.getY());
+            int light = player.world.getLightFor(LightType.SKY, pos);
+
+            lightLevels.add(light);
+            depthLevels.add(depth);
+        }
+
+        double light = CSMath.average(lightLevels.toArray(new Integer[0]));
+        double depth = CSMath.average(depthLevels.toArray(new Double[0]));
+
+        return temp ->
+        {
+            Map<Double, Double> valueMap = new HashMap<>();
+            valueMap.put(temp.get(), 0.8);
+            valueMap.put(CSMath.blend(midTemp, temp.get(), light, 0, 15), 2.0);
+            valueMap.put(CSMath.blend(temp.get(), midTemp, depth, 2, 20), 4.0);
+
+            return new Temperature(CSMath.weightedAverage(valueMap));
+        };
     }
 
     public String getID()
