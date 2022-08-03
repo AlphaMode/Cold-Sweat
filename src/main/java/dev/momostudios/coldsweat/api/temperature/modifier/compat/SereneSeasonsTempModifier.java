adec4d23acc57b5e05e3b63056644318445940ac
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:41:01 -0400

    Migrate SereneSeasonsTempModifier to new config retrieval system

--- src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/compat/SereneSeasonsTempModifier.java
+++ src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/compat/SereneSeasonsTempModifier.java
@@ -1,38 +1,51 @@
 package dev.momostudios.coldsweat.api.temperature.modifier.compat;
 
+import dev.momostudios.coldsweat.config.WorldSettingsConfig;
+import dev.momostudios.coldsweat.util.config.LoadedValue;
 import net.minecraft.entity.player.PlayerEntity;
 import dev.momostudios.coldsweat.api.temperature.Temperature;
 import dev.momostudios.coldsweat.api.temperature.modifier.TempModifier;
 import sereneseasons.api.season.SeasonHelper;
 
+import java.util.function.Function;
+
 public class SereneSeasonsTempModifier extends TempModifier
 {
+    static LoadedValue<Double[]> SUMMER_TEMPS = LoadedValue.of(() -> WorldSettingsConfig.getInstance().summerTemps());
+    static LoadedValue<Double[]> AUTUMN_TEMPS = LoadedValue.of(() -> WorldSettingsConfig.getInstance().autumnTemps());
+    static LoadedValue<Double[]> WINTER_TEMPS = LoadedValue.of(() -> WorldSettingsConfig.getInstance().winterTemps());
+    static LoadedValue<Double[]> SPRING_TEMPS = LoadedValue.of(() -> WorldSettingsConfig.getInstance().springTemps());
+
     @Override
-    public Temperature getResult(Temperature temp, PlayerEntity player)
+    public Function<Temperature, Temperature> calculate(PlayerEntity player)
     {
         if (player.world.getDimensionType().isNatural())
+        {
+            double value;
             switch (SeasonHelper.getSeasonState(player.world).getSubSeason())
             {
-                case EARLY_AUTUMN: return temp.add(0.2);
-                case MID_AUTUMN:   return temp;
-                case LATE_AUTUMN:  return temp.add(-0.2);
+                case EARLY_AUTUMN : value = AUTUMN_TEMPS.get()[0]; break;
+                case MID_AUTUMN   : value = AUTUMN_TEMPS.get()[1]; break;
+                case LATE_AUTUMN  : value = AUTUMN_TEMPS.get()[2]; break;
 
-                case EARLY_WINTER: return temp.add(-0.4);
-                case MID_WINTER:   return temp.add(-0.6);
-                case LATE_WINTER:  return temp.add(-0.4);
+                case EARLY_WINTER : value = WINTER_TEMPS.get()[0]; break;
+                case MID_WINTER   : value = WINTER_TEMPS.get()[1]; break;
+                case LATE_WINTER  : value = WINTER_TEMPS.get()[2]; break;
 
-                case EARLY_SPRING: return temp.add(-0.2);
-                case MID_SPRING:   return temp;
-                case LATE_SPRING:  return temp.add(0.2);
+                case EARLY_SPRING : value = SPRING_TEMPS.get()[0]; break;
+                case MID_SPRING   : value = SPRING_TEMPS.get()[1]; break;
+                case LATE_SPRING  : value = SPRING_TEMPS.get()[2]; break;
 
-                case EARLY_SUMMER: return temp.add(0.4f);
-                case MID_SUMMER:   return temp.add(0.6f);
-                case LATE_SUMMER:  return temp.add(0.4f);
+                case EARLY_SUMMER : value = SUMMER_TEMPS.get()[0]; break;
+                case MID_SUMMER   : value = SUMMER_TEMPS.get()[1]; break;
+                case LATE_SUMMER  : value = SUMMER_TEMPS.get()[2]; break;
 
-                default: return temp;
+                default: return temp -> temp;
             }
+            return temp -> temp.add(value);
+        }
         else
-            return temp;
+            return temp -> temp;
     }
 
     @Override
