8fc28c15d81bf883864390d9e49a0e631d94c692
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:28:47 -0400

    Revert HellLampTempModifier to changing temperature (with new system)

--- src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/HellLampTempModifier.java
+++ src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/HellLampTempModifier.java
@@ -1,14 +1,23 @@
 package dev.momostudios.coldsweat.api.temperature.modifier;
 
+import dev.momostudios.coldsweat.util.config.ConfigCache;
 import net.minecraft.entity.player.PlayerEntity;
 import dev.momostudios.coldsweat.api.temperature.Temperature;
+
+import java.util.function.Function;
 
 public class HellLampTempModifier extends TempModifier
 {
     @Override
-    public Temperature getResult(Temperature temp, PlayerEntity player)
+    public Function<Temperature, Temperature> calculate(PlayerEntity player)
     {
-        return temp.add(0.8);
+        double almostMox = ConfigCache.getInstance().maxTemp * 0.99;
+        return temp ->
+        {
+            if (temp.get() < almostMox) return temp;
+
+            return temp.multiply(Math.max(0.4, almostMox / temp.get()));
+        };
     }
 
     @Override
