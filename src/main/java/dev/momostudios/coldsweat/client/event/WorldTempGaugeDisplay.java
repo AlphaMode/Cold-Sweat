ca2841c0465d103da805cd390f8b1d33e3bf5bca
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:48:57 -0400

    Optimize world temp gauge UI element

--- src/main/java/dev/momostudios/coldsweat/client/event/WorldTempGaugeDisplay.java
+++ src/main/java/dev/momostudios/coldsweat/client/event/WorldTempGaugeDisplay.java
@@ -1,10 +1,12 @@
 package dev.momostudios.coldsweat.client.event;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.momostudios.coldsweat.api.temperature.Temperature;
+import dev.momostudios.coldsweat.common.capability.ITemperatureCap;
 import dev.momostudios.coldsweat.common.capability.ModCapabilities;
 import dev.momostudios.coldsweat.config.ClientSettingsConfig;
-import dev.momostudios.coldsweat.config.ConfigCache;
+import dev.momostudios.coldsweat.util.config.ConfigCache;
 import dev.momostudios.coldsweat.util.math.CSMath;
 import dev.momostudios.coldsweat.util.registries.ModItems;
 import net.minecraft.client.Minecraft;
@@ -23,8 +25,11 @@ import dev.momostudios.coldsweat.api.temperature.Temperature.Units;
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class WorldTempGaugeDisplay
 {
-    private static double PREV_CLIENT_TEMP = 0;
-    public static double CLIENT_TEMP = 0;
+    public static ITemperatureCap PLAYER_CAP = null;
+    public static double WORLD_TEMP = 0;
+    public static double TRUE_WORLD_TEMP = 0;
+    private static double PREV_WORLD_TEMP = 0;
+    static boolean SHOW_WORLD_TEMP = false;
 
     public static double MAX_OFFSET = 0;
     public static double MIN_OFFSET = 0;
@@ -37,11 +42,13 @@ public class WorldTempGaugeDisplay
         PlayerEntity player = Minecraft.getInstance().player;
 
         if (event.getType() == RenderGameOverlayEvent.ElementType.ALL &&
-        (CSMath.isBetween(player.inventory.getSlotFor(new ItemStack(ModItems.THERMOMETER)), 0, 8) ||
-        player.getHeldItemOffhand().getItem()  == ModItems.THERMOMETER || !ConfigCache.getInstance().showWorldTemp))
+        (CSMath.isInRange(player.inventory.getSlotFor(new ItemStack(ModItems.THERMOMETER)), 0, 8) ||
+        player.getHeldItemOffhand().getItem()  == ModItems.THERMOMETER || !ConfigCache.getInstance().requireThermometer))
         {
-            int scaleX = event.getWindow().getScaledWidth();
-            int scaleY = event.getWindow().getScaledHeight();
+            MatrixStack ms = event.getMatrixStack();
+
+            int width = event.getWindow().getScaledWidth();
+            int height = event.getWindow().getScaledHeight();
 
             double min = ConfigCache.getInstance().minTemp - MIN_OFFSET;
             double max = ConfigCache.getInstance().maxTemp + MAX_OFFSET;
@@ -49,7 +56,7 @@ public class WorldTempGaugeDisplay
             boolean bobbing = CLIENT_CONFIG.iconBobbing();
 
             // Get player ambient temperature
-            double temp = CSMath.convertUnits(CLIENT_TEMP, CLIENT_CONFIG.celsius() ? Temperature.Units.C : Temperature.Units.F, Temperature.Units.MC, true);
+            double temp = CSMath.convertUnits(WORLD_TEMP, CLIENT_CONFIG.celsius() ? Temperature.Units.C : Temperature.Units.F, Temperature.Units.MC, true);
 
             // Get the temperature severity
             int severity = getSeverity(temp, min, max);
@@ -66,63 +73,65 @@ public class WorldTempGaugeDisplay
             };
 
             // Set default gauge texture
-            String gaugeLocation = "cold_sweat:textures/gui/overlay/world_temp_gauge/";
-            ResourceLocation gaugeTexture;
-            switch (severity)
-            {
-                case  1: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_hot.png");         break;
-                case  2: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_burning_0.png");   break;
-                case  3: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_burning_1.png");   break;
-                case  4: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_burning_2.png");   break;
-                case -1: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_cold.png");        break;
-                case -2: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_freezing_0.png");  break;
-                case -3: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_freezing_1.png");  break;
-                case -4: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_freezing_2.png");  break;
-                default: gaugeTexture = new ResourceLocation(gaugeLocation + "temp_gauge_normal.png");      break;
-            };
+            ResourceLocation gaugeTexture = new ResourceLocation("cold_sweat:textures/gui/overlay/world_temp_gauge.png");
 
             // Render gauge
-            event.getMatrixStack().push();
+            ms.push();
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
             Minecraft.getInstance().getTextureManager().bindTexture(gaugeTexture);
             RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-            AbstractGui.blit(event.getMatrixStack(), (scaleX / 2) + 94 + CLIENT_CONFIG.tempGaugeX(), scaleY - 19 + CLIENT_CONFIG.tempGaugeY(), 0, 0, 25, 16, 25, 16);
+            AbstractGui.blit(ms, (width / 2) + 94 + CLIENT_CONFIG.tempGaugeX(), height - 19 + CLIENT_CONFIG.tempGaugeY(), 0, 64 - severity * 16, 25, 16, 25, 144);
 
             RenderSystem.disableBlend();
 
             // Sets the text bobbing offset (or none if disabled)
-            int bob = temp > max || temp < min ? (player.ticksExisted % 2 == 0 && bobbing ? 16 : 15) : 15;
+            int bob = bobbing && !CSMath.isInRange(temp, min, max) && player.ticksExisted % 2 == 0 ? 1 : 0;
 
             // Render text
-            int blendedTemp = (int) CSMath.blend(PREV_CLIENT_TEMP, CLIENT_TEMP, Animation.getPartialTickTime(), 0, 1);
+            int blendedTemp = (int) CSMath.blend(PREV_WORLD_TEMP, WORLD_TEMP, Animation.getPartialTickTime(), 0, 1);
 
-            Minecraft.getInstance().fontRenderer.drawString(event.getMatrixStack(), "" + (blendedTemp + CLIENT_CONFIG.tempOffset()) + "",
-            /* X */ scaleX / 2f + 107 + Integer.toString(blendedTemp + CLIENT_CONFIG.tempOffset()).length() * -3 + CLIENT_CONFIG.tempGaugeX(),
-            /* Y */ scaleY - bob + CLIENT_CONFIG.tempGaugeY(), readoutColor);
-            event.getMatrixStack().pop();
+            Minecraft.getInstance().fontRenderer.drawString(ms, "" + (blendedTemp + CLIENT_CONFIG.tempOffset()) + "",
+            /* X */ width / 2f + 107 + Integer.toString(blendedTemp + CLIENT_CONFIG.tempOffset()).length() * -3 + CLIENT_CONFIG.tempGaugeX(),
+            /* Y */ height - 15 - bob + CLIENT_CONFIG.tempGaugeY(), readoutColor);
+            ms.pop();
         }
     }
 
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event)
     {
-        if (Minecraft.getInstance().renderViewEntity instanceof PlayerEntity)
+        if (event.phase == TickEvent.Phase.START && Minecraft.getInstance().renderViewEntity instanceof PlayerEntity)
         {
             PlayerEntity player = (PlayerEntity) Minecraft.getInstance().renderViewEntity;
-            player.getCapability(ModCapabilities.PLAYER_TEMPERATURE).ifPresent(cap ->
+
+            // Ensure player temp capability is stored
+            if (PLAYER_CAP == null || player.ticksExisted % 40 == 0)
+            {
+                PLAYER_CAP = player.getCapability(ModCapabilities.PLAYER_TEMPERATURE).orElse(null);
+            }
+
+            SHOW_WORLD_TEMP = !ConfigCache.getInstance().requireThermometer
+                           || CSMath.isInRange(player.inventory.getSlotFor(new ItemStack(ModItems.THERMOMETER)), 0, 8)
+                           || player.getHeldItemOffhand().getItem()  == ModItems.THERMOMETER;
+
+            if (SHOW_WORLD_TEMP)
             {
                 boolean celsius = CLIENT_CONFIG.celsius();
 
-                double tempReadout = CSMath.convertUnits(cap.get(Temperature.Types.WORLD), Units.MC, celsius ? Units.C : Units.F, true);
-                PREV_CLIENT_TEMP = CLIENT_TEMP;
+                // Get temperature in actual degrees
+                TRUE_WORLD_TEMP = PLAYER_CAP.get(Temperature.Types.WORLD);
+                double realTemp = CSMath.convertUnits(TRUE_WORLD_TEMP, Units.MC, celsius ? Units.C : Units.F, true);
 
-                CLIENT_TEMP = CLIENT_TEMP + (tempReadout - CLIENT_TEMP) / 10.0;
+                // Calculate the blended world temp for this tick
+                PREV_WORLD_TEMP = WORLD_TEMP;
+                WORLD_TEMP += (realTemp - WORLD_TEMP) / 10.0;
 
-                MAX_OFFSET = cap.get(Temperature.Types.MAX);
-                MIN_OFFSET = cap.get(Temperature.Types.MIN);
-            });
+                // Update max/min offset
+                MAX_OFFSET = PLAYER_CAP.get(Temperature.Types.MAX);
+                MIN_OFFSET = PLAYER_CAP.get(Temperature.Types.MIN);
+            }
         }
     }
 
