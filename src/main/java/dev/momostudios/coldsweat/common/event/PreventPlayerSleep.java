2ffa43d7937de94b3f7cfb04d856ad8a7b6250b0
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:39:52 -0400

    Fix player sleep events to be less jank

--- src/main/java/dev/momostudios/coldsweat/common/event/PreventPlayerSleep.java
+++ src/main/java/dev/momostudios/coldsweat/common/event/PreventPlayerSleep.java
@@ -1,7 +1,7 @@
 package dev.momostudios.coldsweat.common.event;
 
 import dev.momostudios.coldsweat.api.temperature.Temperature;
-import dev.momostudios.coldsweat.config.ConfigCache;
+import dev.momostudios.coldsweat.util.config.ConfigCache;
 import dev.momostudios.coldsweat.util.entity.TempHelper;
 import dev.momostudios.coldsweat.util.math.CSMath;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,25 +16,26 @@ public class PreventPlayerSleep
     @SubscribeEvent
     public static void onTrySleep(PlayerSleepInBedEvent event)
     {
+        // There's already something blocking the player from sleeping
+        if (event.getResultStatus() != null) return;
+
         PlayerEntity player = event.getPlayer();
         double bodyTemp = TempHelper.getTemperature(player, Temperature.Types.BODY).get();
         double worldTemp = TempHelper.getTemperature(player, Temperature.Types.WORLD).get();
         double minTemp = ConfigCache.getInstance().minTemp + TempHelper.getTemperature(player, Temperature.Types.MIN).get();
         double maxTemp = ConfigCache.getInstance().maxTemp + TempHelper.getTemperature(player, Temperature.Types.MAX).get();
 
-        if (!CSMath.isBetween((int) bodyTemp, -99, 99))
+        // If the player's body temperature is critical
+        if (!CSMath.isBetween(bodyTemp, -100, 100))
         {
-            player.sendStatusMessage(new TranslationTextComponent("cold_sweat.message.sleep.body",
-            new TranslationTextComponent(bodyTemp > 99 ? "cold_sweat.message.sleep.hot" : "cold_sweat.message.sleep.cold").getString()), true);
-            event.getPlayer().swingArm(event.getPlayer().swingingHand);
-            event.setCanceled(true);
+            player.sendStatusMessage(new TranslationTextComponent("cold_sweat.message.sleep.body." + (bodyTemp > 99 ? "hot" : "cold")), true);
+            event.setResult(PlayerEntity.SleepResult.OTHER_PROBLEM);
         }
-        else if (!CSMath.isBetween(worldTemp, minTemp, maxTemp))
+        // If the player's world temperature is critical
+        else if (!CSMath.isInRange(worldTemp, minTemp, maxTemp))
         {
-            player.sendStatusMessage(new TranslationTextComponent("cold_sweat.message.sleep.world",
-            new TranslationTextComponent(worldTemp > maxTemp ? "cold_sweat.message.sleep.hot" : "cold_sweat.message.sleep.cold").getString()), true);
-            event.getPlayer().swingArm(event.getPlayer().swingingHand);
-            event.setCanceled(true);
+            player.sendStatusMessage(new TranslationTextComponent("cold_sweat.message.sleep.world." + (worldTemp > maxTemp ? "hot" : "cold")), true);
+            event.setResult(PlayerEntity.SleepResult.OTHER_PROBLEM);
         }
     }
 }
