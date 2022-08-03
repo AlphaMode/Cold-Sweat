4c929352edb1e9f329038bbe60c67f17c44b17c9
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:30:49 -0400

    Optimize Hellspring Lamp

--- src/main/java/dev/momostudios/coldsweat/common/item/HellspringLampItem.java
+++ src/main/java/dev/momostudios/coldsweat/common/item/HellspringLampItem.java
@@ -1,15 +1,20 @@
 package dev.momostudios.coldsweat.common.item;
 
 import dev.momostudios.coldsweat.api.temperature.Temperature;
-import dev.momostudios.coldsweat.config.ConfigCache;
+import dev.momostudios.coldsweat.api.temperature.modifier.TempModifier;
+import dev.momostudios.coldsweat.util.config.ConfigCache;
 import dev.momostudios.coldsweat.core.itemgroup.ColdSweatGroup;
+import dev.momostudios.coldsweat.util.config.LoadedValue;
 import dev.momostudios.coldsweat.util.entity.NBTHelper;
+import dev.momostudios.coldsweat.util.math.CSMath;
 import dev.momostudios.coldsweat.util.registries.ModSounds;
 import dev.momostudios.coldsweat.util.world.WorldHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
@@ -17,11 +22,16 @@ import dev.momostudios.coldsweat.api.temperature.modifier.HellLampTempModifier;
 import dev.momostudios.coldsweat.config.ItemSettingsConfig;
 import dev.momostudios.coldsweat.util.entity.TempHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HellspringLampItem extends Item
 {
+    static LoadedValue<List<String>> VALID_DIMENSIONS = LoadedValue.of(() -> new ArrayList<>(ItemSettingsConfig.getInstance().soulLampDimensions()));
+
     public HellspringLampItem()
     {
-        super(new Properties().group(ColdSweatGroup.COLD_SWEAT).maxStackSize(1));
+        super(new Properties().group(ColdSweatGroup.COLD_SWEAT).maxStackSize(1).isImmuneToFire());
     }
 
     @Override
@@ -30,38 +40,33 @@ public class HellspringLampItem extends Item
         if (entityIn instanceof PlayerEntity && !worldIn.isRemote)
         {
             PlayerEntity player = (PlayerEntity) entityIn;
+            double max = ConfigCache.getInstance().maxTemp;
+            TempModifier lampMod = TempHelper.getModifier(player, Temperature.Types.WORLD, HellLampTempModifier.class);
+            double temp = lampMod != null ? lampMod.getLastInput().get() : TempHelper.getTemperature(player, Temperature.Types.WORLD).get();
 
-            // Fuel the item on creation
-            if (!stack.getOrCreateTag().getBoolean("hasTicked"))
-            {
-                stack.getOrCreateTag().putBoolean("hasTicked", true);
-                setFuel(stack, 64);
-            }
-
-            boolean validDimension = false;
-            for (String id : ItemSettingsConfig.getInstance().hellLampDimensions())
-            {
-                if (worldIn.getDimensionKey().getLocation().toString().equals(id))
-                {
-                    validDimension = true;
-                    break;
-                }
-            }
-
-            if ((isSelected || player.getHeldItemOffhand() == stack) && validDimension && getFuel(stack) > 0
-            && TempHelper.getTemperature(player, Temperature.Types.WORLD).get() > ConfigCache.getInstance().maxTemp)
+            if ((isSelected || player.getHeldItemOffhand() == stack) && temp > max && getFuel(stack) > 0
+            && VALID_DIMENSIONS.get().contains(worldIn.getDimensionKey().getLocation().toString()))
             {
                 // Drain fuel
                 if (player.ticksExisted % 10 == 0 && !(player.isCreative() || player.isSpectator()))
-                    addFuel(stack, -0.02f);
+                {
+                    addFuel(stack, -0.02d * CSMath.clamp(temp - ConfigCache.getInstance().maxTemp, 1d, 3d));
+                }
 
                 // Give effect to nearby players
-                AxisAlignedBB bb = new AxisAlignedBB(player.getPosX() - 3.5, player.getPosY() - 3.5, player.getPosZ() - 3.5,
-                                                     player.getPosX() + 3.5, player.getPosY() + 3.5, player.getPosZ() + 3.5);
-                worldIn.getEntitiesWithinAABB(PlayerEntity.class, bb).forEach(e ->
+                if (player.ticksExisted % 5 == 0)
                 {
-                    TempHelper.insertModifier(e, new HellLampTempModifier().expires(5), Temperature.Types.MAX);
-                });
+                    AxisAlignedBB bb = new AxisAlignedBB(player.getPosX() - 3.5, player.getPosY() - 3.5, player.getPosZ() - 3.5,
+                                                         player.getPosX() + 3.5, player.getPosY() + 3.5, player.getPosZ() + 3.5);
+                    for (PlayerEntity entity : worldIn.getEntitiesWithinAABB(PlayerEntity.class, bb))
+                    {
+                        HellLampTempModifier modifier = TempHelper.getModifier(entity, Temperature.Types.WORLD, HellLampTempModifier.class);
+                        if (modifier != null)
+                            modifier.expires(modifier.getTicksExisted() + 5);
+                        else
+                            TempHelper.addModifier(entity, new HellLampTempModifier().expires(5).tickRate(5), Temperature.Types.WORLD, false);
+                    }
+                }
 
                 // If the conditions are met, turn on the lamp
                 if (stack.getOrCreateTag().getInt("stateChangeTimer") <= 0 && !stack.getOrCreateTag().getBoolean("isOn"))
@@ -98,16 +103,28 @@ public class HellspringLampItem extends Item
         return slotChanged;
     }
 
-    private void setFuel(ItemStack stack, float fuel)
+    private void setFuel(ItemStack stack, double fuel)
     {
-        stack.getOrCreateTag().putFloat("fuel", fuel);
+        stack.getOrCreateTag().putDouble("fuel", fuel);
     }
-    private void addFuel(ItemStack stack, float fuel)
+    private void addFuel(ItemStack stack, double fuel)
     {
         setFuel(stack, getFuel(stack) + fuel);
     }
     private float getFuel(ItemStack stack)
     {
         return stack.getOrCreateTag().getFloat("fuel");
+    }
+
+    @Override
+    public void fillItemGroup(ItemGroup tab, NonNullList<ItemStack> itemList)
+    {
+        if (this.isInGroup(tab))
+        {
+            ItemStack stack = new ItemStack(this);
+            stack.getOrCreateTag().putBoolean("isOn", true);
+            stack.getOrCreateTag().putDouble("fuel", 64);
+            itemList.add(stack);
+        }
     }
 }
