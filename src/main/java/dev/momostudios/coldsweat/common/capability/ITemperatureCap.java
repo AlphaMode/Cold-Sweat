5d6410dad513e2614a45b2b5bec190fe36537c40
mikulgamour <gamer3mc@gmail.com>
2022-08-03 00:33:12 -0400

    ITemperatureCap serialize own NBT

--- src/main/java/dev/momostudios/coldsweat/common/capability/ITemperatureCap.java
+++ src/main/java/dev/momostudios/coldsweat/common/capability/ITemperatureCap.java
@@ -3,6 +3,7 @@ package dev.momostudios.coldsweat.common.capability;
 import dev.momostudios.coldsweat.api.temperature.Temperature;
 import dev.momostudios.coldsweat.api.temperature.modifier.TempModifier;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 
 import java.util.List;
 
@@ -14,6 +15,9 @@ public interface ITemperatureCap
     boolean hasModifier(Temperature.Types type, Class<? extends TempModifier> mod);
     void clearModifiers(Temperature.Types type);
     void copy(ITemperatureCap cap);
-    void tickUpdate(PlayerEntity player);
-    void tickClient(PlayerEntity player);
+    void tick(PlayerEntity player);
+    void tickDummy(PlayerEntity player);
+
+    CompoundNBT serializeNBT();
+    void deserializeNBT(CompoundNBT tag);
 }
