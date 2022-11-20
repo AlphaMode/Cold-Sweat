6096f2b9fbc8340c5d25f5e23ce5f9ed54139003
mikulgamour <gamer3mc@gmail.com>
2022-11-20 13:55:33 -0500

    Better minecart drop mixin

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinMinecart.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinMinecart.java
@@ -9,21 +9,26 @@ import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractMinecartEntity.class)
 public class MixinMinecart
 {
     AbstractMinecartEntity minecart = (AbstractMinecartEntity) (Object) this;
 
-    @Inject(method = "killMinecart(Lnet/minecraft/util/DamageSource;)V",
-            at = @At("HEAD"), remap = ColdSweat.REMAP_MIXINS)
-    public void killMinecart(DamageSource source, CallbackInfo ci)
+    @Inject(method = "attackEntityFrom(Lnet/minecraft/util/DamageSource;F)Z",
+            at = @At
+            (
+                value = "INVOKE",
+                target = "Lnet/minecraft/entity/item/minecart/AbstractMinecartEntity;killMinecart(Lnet/minecraft/util/DamageSource;)V"
+            ),
+            remap = ColdSweat.REMAP_MIXINS,
+            cancellable = true)
+    public void killMinecart(DamageSource source, float amount, CallbackInfoReturnable<Boolean> ci)
     {
         ItemStack carryStack = minecart.getDisplayTile().getBlock().asItem().getDefaultInstance();
         if (!carryStack.isEmpty())
         {
-            minecart.remove();
             if (minecart.world.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS))
             {
                 ItemStack itemstack = new ItemStack(Items.MINECART);
@@ -35,6 +40,7 @@ public class MixinMinecart
                 minecart.entityDropItem(itemstack);
                 minecart.entityDropItem(carryStack);
             }
+            minecart.remove();
             ci.cancel();
         }
     }
